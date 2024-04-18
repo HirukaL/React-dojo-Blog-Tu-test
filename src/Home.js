d8edf07a2146176ad06b1ad74bef3b77bce57023
Hiruka Lakshan <ict20090@std.uwu.ac.lk>
2024-04-18 18:19:45 +0530

    tutorial 05

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Hwllow, Guys');
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hellow' + name, e.target);
+    }
     return (  
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick ={(e) => handleClickAgain('Hiruka', e)}>Click me again</button>
         </div>
     );
 }
