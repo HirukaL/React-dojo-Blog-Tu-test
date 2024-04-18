279bd269d673771e65f4c75be69ed99b8a8a9ad4
Hiruka Lakshan <ict20090@std.uwu.ac.lk>
2024-04-18 16:03:53 +0530

    tutorial 02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the Blog';
+  const likes = 50;
+  const link = "https://www.google.com";
+
+
   return (
     <div className="App">
       <div className='Content'>
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Here Likes {likes} times</p>
+        <p>{50}</p>
+        <p>{ "hellow Guys" }</p>
+        <p>Number Test:{ [ 1,2,3,4,5 ] }</p>
+        <p>{ Math.random() * 50}</p>
+        <a href={link}>Google Search</a>
       </div>
     </div>
   );
